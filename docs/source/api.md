0476a33d3a0bc6baf0e1b822f89a8f034a8d5147
Jonathan Gray <jonathan.gray@flowminder.org>
2024-12-17 12:40:40 +0000

    Removed everything with mike 2.1.3

--- docs/source/api.md
+++
@@ -1,4 +0,0 @@
-# API
-
-::: flowpytertask.flowpytertask
-	handler: python
--- src/flowpytertask/plugins/__init__.py
+++
@@ -1,8 +0,0 @@
-from airflow.plugins_manager import AirflowPlugin
-
-from . import base64_jinja
-
-
-class FptMacros(AirflowPlugin):
-    name = "flowpytertask"
-    macros = [base64_jinja.b64_encode, base64_jinja.dump_yaml]
